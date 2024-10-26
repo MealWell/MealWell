e68df072deb2603887b395b38d36614a90cb29a4
Denis Rusu <denis@globalsoftsys.ro>
2024-10-26 23:41:52 +0300

    Milestone 2. Solution

--- src/components/custom/milestone2/Milestone2.jsx
+++ src/components/custom/milestone2/Milestone2.jsx
@@ -1,9 +1,11 @@
 import IdentifyingProblem from "./IdentifyingProblem";
+import Solution from "./Solution";
 
 const Milestone2 = () => {
   return (
-    <div className="space-y-2">
+    <div className="space-y-6">
       <IdentifyingProblem />
+      <Solution />
     </div>
   );
 };
---
+++ src/components/custom/milestone2/Solution.jsx
@@ -0,0 +1,21 @@
+const Solution = () => {
+  return (
+    <div>
+      <h1 className="mb-5 text-5xl bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text">
+        Soluția Propusă
+      </h1>
+      <div className="text-xl text-justify">
+        <p>
+          Considerăm că o soluție eficientă pentru problema alimentației
+          sănătoase într-un program aglomerat este dezvoltarea unei aplicații
+          care să ofere mese personalizate și livrare la domiciliu. Această
+          aplicație ar permite utilizatorilor să aleagă planuri de mese adaptate
+          nevoilor și preferințelor lor, inclusiv opțiuni pentru diferite diete,
+          cum ar fi cele bogate în proteine, vegetariene sau fără gluten.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Solution;
