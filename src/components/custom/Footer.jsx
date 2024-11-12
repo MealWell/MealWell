fbae524df69ea2f06e9726cff685d50ed74e780c
vanyadarkov <phonedarkov@gmail.com>
2024-11-12 12:06:42 +0200

    Changed icon of mealwell to chef hat

--- src/components/custom/Footer.jsx
+++ src/components/custom/Footer.jsx
@@ -1,12 +1,12 @@
 import { TypographyH1 } from "@/components/custom/typography/TypographyH1.jsx";
-import { GiHotMeal } from "react-icons/gi";
+import { ChefHat } from "lucide-react";
 
 const Footer = () => {
   return (
     <div
       className={`w-full h-[20dvh] bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 flex flex-row justify-center items-center rounded-t-2xl space-y-4 mt-10 gap-x-8`}
     >
-      <GiHotMeal className={"text-6xl md:text-9xl"} />
+      <ChefHat className={"size-16 md:size-32"} />
       <TypographyH1>MealWell</TypographyH1>
     </div>
   );
